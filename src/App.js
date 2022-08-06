8e3907acb6605bcccb96c38ebe5d34f4a3dd999a
Bahti140 <1400707@gmail.com>
2022-08-06 16:46:00 +0500

    last

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
+import { Routes, Route} from 'react-router-dom';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Container from "@mui/material/Container";
-
 import { Header } from "./components";
 import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+import { fetchAuthMe, selectIsAuth } from './redux/slices/auth';
+
+
 
 function App() {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+
+  React.useEffect(() => {
+    dispatch(fetchAuthMe()); 
+
+  }, []);
+
   return (
     <>
       <Header />
       <Container maxWidth="lg">
-        <Home />
-        {/*<FullPost />*/}
-        {/*<AddPost />*/}
-        {/*<Login />*/}
-        {/*<Registration />*/}
+        <Routes>
+          <Route to path='/' element={<Home />}  />
+          <Route to path='/posts/:id' element={<FullPost />}  />
+          <Route to path='/posts/:id/edit' element={<AddPost />}  />
+          <Route to path='/add-post' element={<AddPost />}  />
+          <Route to path='/login' element={<Login />}  />
+          <Route to path='/register' element={<Registration />}  />      
+        </Routes>        
       </Container>
     </>
   );
--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -4,10 +4,47 @@ import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
+import { Navigate } from 'react-router-dom';
+import { fetchAuth, fetchRegister, selectIsAuth } from "../../redux/slices/auth";
+import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 import styles from './Login.module.scss';
 
 export const Registration = () => {
+  
+  const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
+  const { 
+    register, 
+    handleSubmit, 
+    formState: {errors, isValid},
+   } = useForm({
+    defaultValues: {
+      fullName: 'Vasya Pupkin',
+      email: 'vasya@test.com',
+      password: '1234'
+    },
+    mode: 'onChange',
+  });
+
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchRegister(values));
+
+    if (!data.payload) {
+      return alert('Could not register!')
+    }
+
+    if ('token' in data.payload) {
+      window.localStorage.setItem('token', data.payload.token);
+    } 
+  };
+
+  if (isAuth) {
+    return <Navigate to='/' />;
+  }
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
@@ -16,12 +53,39 @@ export const Registration = () => {
       <div className={styles.avatar}>
         <Avatar sx={{ width: 100, height: 100 }} />
       </div>
-      <TextField className={styles.field} label="Полное имя" fullWidth />
-      <TextField className={styles.field} label="E-Mail" fullWidth />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField 
+          error={Boolean(errors.fullName?.message)}
+          helperText={errors.fullName?.message}        
+          {...register('fullName', {required: 'Input Your Name'})} 
+          className={styles.field} 
+          label="Полное имя" 
+          fullWidth />
+
+        <TextField  
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          type="email"
+          {...register('email', {required: 'Input Your Email'})} 
+          className={styles.field} 
+          label="E-Mail" 
+          fullWidth />
+
+        <TextField 
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          type="password"
+          {...register('password', {required: 'Input Your Password'})} 
+          className={styles.field} 
+          label="Пароль" 
+          fullWidth />    
+
+      
+
+        <Button disabled={!isValid} type="submit" size="large" variant="contained" fullWidth>
         Зарегистрироваться
-      </Button>
+        </Button>
+      </form>
     </Paper>
   );
 };
